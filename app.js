a0b0d74bbe95c8380b756393af3b6587219ec20d
berkmahim <berkmahim620@gmail.com>
2023-01-28 17:11:39 +0300

    Static Files, EJS

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 import express from 'express'
+import ejs from 'ejs'
 
 const app = express()
 const port = 3000
 const blog = { id: 1, title: "Blog title", description: "Blog description" }
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
+
 
 app.get('/', (req, res) => {
-    res.send(blog)
+    res.render('index')
 })
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+app.get('/addPost', (req, res) => {
+    res.render('add_post')
+})
+app.get('/post', (req, res) => {
+    res.render('post')
+})
+
+
+
+
+
+
 
 app.listen(port, () => {
     console.log(`sunuc ${port} portunda başlatildi`)
